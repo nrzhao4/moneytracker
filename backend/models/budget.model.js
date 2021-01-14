03f7f6dd7c06f6ce558f6f805bb52264629c1ff1
Nina Zhao <nina.r.zhao@gmail.com>
2021-01-14 14:18:22 -0500

    budget has type to distinguish weekly, monthly, or annual budget

--- backend/models/budget.model.js
+++ backend/models/budget.model.js
@@ -4,9 +4,8 @@ const Schema = mongoose.Schema;
 
 const budgetSchema = new Schema(
   {
-    dateStart: { type: Date, required: true },
-    dateEnd: { type: Date, required: true },
     budgetName: { type: String, required: true },
+    budgetType: { type: String, required: true },
     description: { type: String, required: false },
     amount: { type: Number, required: true },
   },
